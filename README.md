1cfd20e408907d3b50cd445c438aa0bfff7673a1
ahamula <andriihamula@gmail.com>
2024-01-26 16:00:20 +0200

    update readme, add NoticeableRow composable

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Jetpack compose sample with common components - bottom navigation bar, hamburger menu + nav drawer, morph animation
+Jetpack compose sample with bottom bar navigation, side / navigation drawer navigation.
+
+Bottom bar navigation consists of:
+- 1st tab: dog list + dog details
+- 2nd tab: dog list + dog place. On dog place screen mapbox sdk used to show dog location.
+- 3rd tab: simple text
+
+Bottom bar composable has custom clipping logic for animation when item clicked.
+
+Side drawer consists of same 1st tab and mocked texts on 2nd and 3rd tabs.
+Also side drawer contains fix for first draw pass offset issue on ModalNavDrawer, and synchronizes hamburger menu "morph" animation with drawer progress.
+
+Exclusive features composable contains NoticeableRow composable that ensure that item at the end is always partially shown, so user understand that NoticeableRow is scrollable.
--- app/src/main/java/ua/com/underlake/ui/main/ExclusiveFeatures.kt
+++ app/src/main/java/ua/com/underlake/ui/main/ExclusiveFeatures.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.RadioButton
@@ -18,13 +19,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.Placeable
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import kotlinx.parcelize.Parcelize
 import ua.com.underlake.ui.AppTheme
 
 @Parcelize
-data class Feature(val title: String): Parcelable
+data class Feature(val title: String) : Parcelable
 
 @Composable
 fun ExclusiveFeatures(
@@ -41,7 +46,8 @@ fun ExclusiveFeatures(
                 .horizontalScroll(rememberScrollState()),
             horizontalArrangement = Arrangement.spacedBy(10.dp)
         ) {
-            for (feature in features) {
+            NoticeableRow(countItems = features.size) {
+                val feature = features[it]
                 Row(
                     modifier = Modifier
                         .border(1.dp, Color.Gray, CircleShape)
@@ -57,6 +63,88 @@ fun ExclusiveFeatures(
             }
         }
     }
+}
+
+@Composable
+fun NoticeableRow(
+    countItems: Int,
+    itemContent: @Composable (Int) -> Unit
+) {
+    val configuration = LocalConfiguration.current
+    //TODO use fillMaxWidth with Layout + parent width
+    val containerWidthDp = configuration.screenWidthDp.dp - 20.dp
+
+    Layout(
+        modifier = Modifier
+            .width(containerWidthDp)
+            .horizontalScroll(rememberScrollState()),
+        measurePolicy = { measurables, constraints ->
+            val placeables = arrayOfNulls<Placeable?>(measurables.size)
+            val parentWidth = constraints.minWidth
+            val measureChildConstraints = Constraints()
+            var childrenIntrinsicWidth = 0
+            var paddingBetweenChildren = 0
+            var isPaddingCalculated = false
+            var maxHeight = 0
+            var containerWidth = 0
+            for ((index, measurable) in measurables.withIndex()) {
+                val placeable = measurable.measure(measureChildConstraints).also {
+                    placeables[index] = it
+                    maxHeight = maxOf(maxHeight, it.height)
+                    containerWidth += it.width
+                }
+                if (isPaddingCalculated) {
+                    continue
+                }
+
+                when {
+                    index == 0 && placeable.width > parentWidth -> {
+                        paddingBetweenChildren = 0
+                        isPaddingCalculated = true
+                    }
+
+                    index == 1 && childrenIntrinsicWidth + placeable.width / 2 > parentWidth -> {
+                        paddingBetweenChildren = 0
+                        isPaddingCalculated = true
+                    }
+
+                    childrenIntrinsicWidth + placeable.width / 2 == parentWidth -> {
+                        paddingBetweenChildren = 0
+                        isPaddingCalculated = true
+                    }
+
+                    childrenIntrinsicWidth + placeable.width / 2 > parentWidth -> {
+                        paddingBetweenChildren =
+                            (parentWidth - childrenIntrinsicWidth + placeables[index - 1]!!.width / 2) / (index - 1)
+                        isPaddingCalculated = true
+                    }
+                }
+
+                childrenIntrinsicWidth += placeable.width
+            }
+
+            if (!isPaddingCalculated) {
+                paddingBetweenChildren =
+                    (parentWidth - childrenIntrinsicWidth + placeables[placeables.size - 1]!!.width / 2) / (placeables.size - 1)
+            }
+
+            containerWidth += (measurables.size - 1) * paddingBetweenChildren
+
+            var nextChildPadding = 0
+
+            layout(containerWidth, maxHeight) {
+                placeables.forEach {
+                    val placeable = it!!
+                    placeable.place(nextChildPadding, (maxHeight - placeable.height) / 2)
+                    nextChildPadding += placeable.width + paddingBetweenChildren
+                }
+            }
+        },
+        content = {
+            for (i in 0 until countItems) {
+                itemContent(i)
+            }
+        })
 }
 
 @Preview(showSystemUi = true)
